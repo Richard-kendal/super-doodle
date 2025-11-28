af6b63b87f0eb4bbe0677d0f9452086294fac8ff
Richard-kendal <156463336+Richard-kendal@users.noreply.github.com>
2025-11-28 18:49:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -373,6 +373,6 @@ def run_telegram_bot():
 threading.Thread(target=run_telegram_bot, daemon=True).start()
 
 # === Запуск ===
-if __namename__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
